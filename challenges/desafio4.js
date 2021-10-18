0974caf61ab29b6420172e83db85bfc673062cfd
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-10-18 16:27:31 -0300

    adiciona desafio 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,11 @@
+db.movies.aggregate([
+  { $addFields: {
+    title_split: { $split: ["$title", " "] },
+  } },
+  { $match: {
+    title_split: { $size: 1 },
+  } },
+  { $sort: {
+    title: 1,
+  } },
+]);
